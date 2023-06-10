9325a6e69868d3692e02e1021d4fb96b543374f5
Patricia E. Miguel <contacto@patriciaemiguel.com>
2023-06-10 13:00:18 -0300

    :sparkles: feat: add Message model and unit test

--- api/models.py
+++ api/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Message(models.Model):
+    """Telegram group message"""
+    sender_id = models.PositiveIntegerField()
+    text = models.CharField(max_length=255)
+    date = models.DateField()
+    message_id = models.PositiveIntegerField()
+    post_author = models.CharField(max_length=255)
+    views = models.PositiveIntegerField()
+    peer_id_channel_id = models.PositiveIntegerField()
+
+    def __str__(self):
+        return self.message_id
