51971ff8cdd2d25402536fd6c029455d6c25b1e0
Patricia E. Miguel <contacto@patriciaemiguel.com>
2023-06-12 20:48:49 -0300

    :see_no_evil: chore: remove unused database settings

--- telegram_api/settings.py
+++ telegram_api/settings.py
@@ -74,22 +74,13 @@ WSGI_APPLICATION = "telegram_api.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
-# DATABASES = {
-#      "default": {
-#         "ENGINE": "django.db.backends.sqlite3",
-#         "NAME": BASE_DIR / "db.sqlite3",
-#     }
-# }
-
-
-DATABASES = {    
+DATABASES = {
    "default": {        
         "ENGINE": "django.db.backends.postgresql",
         "NAME": "telegram",
         "USER": "postgres",
         "PASSWORD": "postgres",
-        "HOST": '127.0.0.1',
+        "HOST": 'db',
         "PORT": 5432,
     }
 }
