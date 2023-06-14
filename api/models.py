4a87a6928caba93116a8133aa344eed52a3be03c
Patricia E. Miguel <contacto@patriciaemiguel.com>
2023-06-14 12:21:24 -0300

    :sparkles: feat: change model fields

--- api/models.py
+++ api/models.py
@@ -3,13 +3,12 @@ from django.db import models
 
 class Message(models.Model):
     """Telegram group message"""
-    sender_id = models.PositiveIntegerField()
-    text = models.CharField(max_length=255)
-    date = models.DateField()
     message_id = models.PositiveIntegerField()
+    date = models.DateField()
     post_author = models.CharField(max_length=255)
-    views = models.PositiveIntegerField()
-    peer_id_channel_id = models.PositiveIntegerField()
+    from_id = models.CharField(max_length=255)
+    text = models.CharField(max_length=255)
+    message = models.CharField(max_length=255)
 
     def __str__(self):
         return self.message_id
